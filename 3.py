cfe7556905a5a5a34b84015e45af38683a1ff390
NoonWood <gkk978@gmail.com>
2017-02-04 13:34:56 +0300

    3 ref

--- 3.py
+++ 3.py
@@ -5,17 +5,15 @@ while 1:
     Num = input('Введите номер дебетовой карты (16 цифр)')
     try:
         intNum = int(Num)
-        #break
     except ValueError:
         print('Вводите числа')
-    finally:
+    else:
         try:
             strNum = str(Num)
             if len(strNum)!= 16:
                 raise Err
         except Err:
             print('Введите 16 цифр')
-         #   break
         else:
             print('Номер>> ',strNum[0:4],'*' * 12,strNum[11:15])
             break
