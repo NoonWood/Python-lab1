0ca0ca39fdd021f2f48c59ea206480c912ab73d2
NoonWood <gkk978@gmail.com>
2017-02-08 10:56:50 +0300

    chang 1

--- 1.py
+++ 1.py
@@ -1,13 +1,17 @@
 class Err(Exception):
     pass
-
-print('Введите число')
-try:
-    a = float(input())
-    if a<0:
-        raise Err
-except Err:
-    print('Некорректный формат!')
-else:
-    b = int((a - int(a))*100)
-    print (int(a), "Руб.", b, "Коп.")
+while 1:
+    print('Введите число')
+    try:
+        a = float(input())
+    except ValueError:
+        print('<Вводите числа!>')
+    else:
+        try:
+            if a<0:
+                raise Err
+        except Err:
+            print('Некорректный формат!')
+        else:
+            b = int((a - int(a))*100)
+            print (int(a), "Руб.", b, "Коп.")
